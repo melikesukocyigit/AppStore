8b065eacd5f4522a69e924c16c269029a4d415b0
melikesukocyigit <melikesukocyigt@hotmail.com>
2025-11-26 16:50:39 +0300

    TodayDetail gesture işlemi yapıldı

---
+++ AppStore/Controller/Search/SearchViewController.swift
@@ -0,0 +1,106 @@
+//
+//  SearchViewController.swift
+//  AppStore
+//
+//  Created by Melike Su KOÇYİĞİT on 21.11.2025.
+//
+import UIKit
+private let reuseIdentifier = "SearchCell"
+class SearchViewController: UICollectionViewController {
+    //Properties
+    var searchResults: [Result] = []{
+        didSet {
+            collectionView.reloadData()
+        }
+    }
+    private let centerLabel: UILabel = {
+         let label = UILabel()
+          label.text = "Enter the name of the application you are looking for.."
+          label.textAlignment = .center
+          label.numberOfLines = 0
+          return label
+      }()
+    //Lifecycle
+   init() {
+        super.init(collectionViewLayout: UICollectionViewFlowLayout())
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        style()
+        layout()
+        
+    }
+}
+// Helpers
+
+extension SearchViewController {
+    
+    private func style() {
+        collectionView.register(SearchCell.self, forCellWithReuseIdentifier: reuseIdentifier)
+        let searchController = UISearchController(searchResultsController: nil)
+        self.navigationItem.searchController = searchController
+        self.navigationItem.hidesSearchBarWhenScrolling = false
+        searchController.searchBar.delegate = self
+        centerLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        
+    }
+    private func layout() {
+        view.addSubview(centerLabel)
+        NSLayoutConstraint.activate([
+            centerLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            centerLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 32),
+            view.trailingAnchor.constraint(equalTo: centerLabel.trailingAnchor,constant: 32)
+            
+            ])
+    }
+}
+
+// UICollectionViewDataSource
+extension SearchViewController {
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        centerLabel.isHidden = self.searchResults.count != 0
+        /*return 5*/ // 5 tane hücre olmasını istiyorum
+        return searchResults.count
+    }
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as! SearchCell
+        cell.result = self.searchResults[indexPath.row]
+        return cell
+    }
+}
+// UICollectionViewDelegateFlowLayout
+extension SearchViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return .init(width: view.frame.width, height: 250)
+    }
+    //searchbarla altındaki collectiıonviewdakiler birbirine çok yakındı uzaklaştırdık
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
+        return .init(width: 0,  height: 10)
+    }
+}
+// UICollectionVİewDelegate
+extension SearchViewController {
+    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let id = String(self.searchResults[indexPath.row].trackId)
+        let controller = AppInfoViewController(id: id)
+        self.navigationController?.pushViewController(controller, animated: true)
+    }
+}
+
+// UISearchBarDelegate
+extension SearchViewController: UISearchBarDelegate {
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        /*print(searchText)*/ // her yazdığımızı yazdıracak konsolda
+        SearchService.fetchData(searchtext: searchText) { results in
+            self.searchResults = results
+        }
+    }
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        self.searchResults = []
+    }
+}
---
+++ AppStore/View/App/AppCellDetailCell.swift
@@ -0,0 +1,104 @@
+//
+//  AppCellDetailCell.swift
+//  AppStore
+//
+//  Created by Melike Su KOÇYİĞİT on 24.11.2025.
+//
+
+import UIKit
+import Kingfisher
+protocol AppCellDetailCellProtocol: AnyObject {
+    func goAppInfoViewController(id:String)
+}
+class AppCellDetailCell: UICollectionViewCell {
+    
+    // properties
+    weak var delegate : AppCellDetailCellProtocol?
+    var result: FeedResult? {
+        didSet {
+            configure()
+        }
+    }
+    private let appIcon:UIImageView = {
+        let imageView = UIImageView()
+        imageView.customMode()
+        imageView.backgroundColor = .systemPurple
+        return imageView
+    }()
+    private let nameLabel:UILabel = {
+        let label = UILabel()
+        label.text = "App Name"
+        return label
+    }()
+    private let firmLabel:UILabel = {
+        let label = UILabel()
+        label.text = "Firm Label"
+        return label
+    }()
+    private lazy var getButton:UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Get", for: .normal)
+        button.setTitleColor(UIColor.blue, for: .normal)
+        button.backgroundColor = UIColor(white: 0.88, alpha: 1)
+        return button
+    }()
+    var labelStackView: UIStackView!
+    var fullStackView: UIStackView!
+    // lifecycle
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        style()
+        layout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+// actions
+extension AppCellDetailCell {
+    @objc private func handleSelf(){
+        delegate?.goAppInfoViewController(id: result!.id ?? "")
+    }
+    
+}
+
+
+// helpers
+extension AppCellDetailCell {
+    private func style() {
+        labelStackView = UIStackView(arrangedSubviews: [nameLabel, firmLabel])
+        labelStackView.axis = .vertical
+        fullStackView = UIStackView(arrangedSubviews: [appIcon,labelStackView,getButton])
+        fullStackView.axis = .horizontal
+        fullStackView.alignment = .center
+        fullStackView.spacing = 8
+        fullStackView.translatesAutoresizingMaskIntoConstraints = false
+        getButton.layer.cornerRadius = 34 / 2
+        appIcon.layer.cornerRadius = 10
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleSelf))
+        self.addGestureRecognizer(tapGesture)
+        
+    }
+    private func layout() {
+        addSubview(fullStackView)
+        NSLayoutConstraint.activate([
+            appIcon.widthAnchor.constraint(equalToConstant: 80),
+            appIcon.heightAnchor.constraint(equalToConstant: 65),
+            getButton.widthAnchor.constraint(equalToConstant: 80),
+            getButton.heightAnchor.constraint(equalToConstant: 34),
+            fullStackView.topAnchor.constraint(equalTo: topAnchor),
+            fullStackView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            fullStackView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            fullStackView.bottomAnchor.constraint(equalTo: bottomAnchor),
+            
+        ])
+    }
+    private func configure() {
+        guard let result = self.result else { return }
+        let viewModel = AppCellDetailCellViewModel(result: result)
+        self.nameLabel.text = viewModel.name
+        self.firmLabel.text = viewModel.artistName
+        self.appIcon.kf.setImage(with: viewModel.appImageUrl)
+    }
+}
